930398df2a7e34e4a0b9efcf4fadfa29f54aa037
Saranya2024 <pji.saranya.madivada@kalvium.community>
2024-05-10 22:29:45 +0530

    Done

---
+++ hooks/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UseState from './component/UseState'; 
+
+function App() {
+  return (
+    <div className="App">
+      <UseState />
+    </div>
+  );
+}
+
+export default App;
